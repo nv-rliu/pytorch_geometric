e2d4e45ef8f85a19520de7010ec008d4e8358753
Ralph Liu <ralphl@nvidia.com>
2025-08-19 09:04:56 -0700

    Stored changes

--- examples/llm/txt2kg_rag.py
+++ examples/llm/txt2kg_rag.py
@@ -31,7 +31,8 @@ from torch.nn.utils import clip_grad_norm_
 from tqdm import tqdm
 
 from torch_geometric import seed_everything
-from torch_geometric.loader import DataLoader, RAGQueryLoader
+from torch_geometric.loader import DataLoader, RAGQueryLoader 
+from torch.utils.data import Subset
 from torch_geometric.nn import (
     GAT,
     LLM,
@@ -151,13 +152,6 @@ def parse_args():
         '--use_x_percent_corpus', default=100.0, type=float,
         help="Debug flag that allows user to only use a random percentage "
         "of available knowledge base corpus for RAG")
-    parser.add_argument(
-        '--store_eval_tuples', action="store_true",
-        help="Store tuple answers from test step to .pkl file for evaluation")
-    parser.add_argument(
-        '--use_stored_eval_tuples', action="store_true",
-        help="Retrieve previously saved tuples for eval instead of generating"
-        "new ones")
     args = parser.parse_args()
 
     assert args.NV_NIM_KEY, "NVIDIA API key is required for TXT2KG and eval"
@@ -214,38 +208,9 @@ prompt_template = """
     
 #####
 def dump_eval_results(args, res):
-    json_path = Path(args.dataset / "eval_dump.json")
-    if json_path.exists():
-        while True:
-            c = input(f" ==> Do you want to delete '{str(json_path)}'? (y/n): ").strip().lower()
-            if c == "y":
-                print(f"   Removing {str(json_path)}")
-                os.remove(json_path)
-                break
-            elif c == "n":
-                return
-            else:
-                print("Please enter y/n.")
-    res['args'] = vars(args)
-    
+    json_path = Path(args.dataset) / "eval_dump.json"
     with open(json_path, 'w') as f:
-        json.dump(res, json_path, indent=2)
-
-def _store_tuples(eval_tuples):
-    with open('tuples.pkl', 'wb') as f:
-        pickle.dump(eval_tuples, f)
-
-def _get_stored_tuples():
-    tuples = None
-    with open('tuples.pkl', 'rb') as f:
-        tuples = pickle.load(f)
-
-    if tuples is None:
-        raise FileNotFoundError(
-            "Error: Could not open stored tuples. Have you checked that "
-            "tuples.pkl exists?")
-
-    return tuples
+        json.dump(res, f, indent=2)
 #####
 
 
@@ -543,7 +508,7 @@ def make_dataset(args):
     extracted_triple_sizes = []
     global max_chars_in_train_answer
     for data_point in tqdm(qa_pairs, desc="Building un-split dataset"):
-        if data_point["is_impossible"]:
+        if data_point.get("is_impossible"):
             continue
         QA_pair = (data_point["question"], data_point["answer"])
         q = QA_pair[0]
@@ -736,46 +701,68 @@ def test(model, test_loader, args):
         return llm_judge.score(question, pred, correct_answer)
 
     eval_tuples = []
-    if not args.use_stored_eval_tuples:
-        for i, test_batch in enumerate(tqdm(test_loader, desc="Testing")):
-            # TODO Remove
-            if i > 10:
-                break
-            ###############
-            
-            new_qs = []
-            raw_qs = test_batch["question"]
-            for i, q in enumerate(test_batch["question"]):
-                # insert VectorRAG context
-                new_qs.append(
-                    prompt_template.format(
-                        question=q, context="\n".join(test_batch.text_context[i])))
-            test_batch.question = new_qs
-            if args.skip_graph_rag:
-                test_batch.desc = ""
-            preds = (inference_step(model, test_batch,
-                                    max_out_tokens=max_chars_in_train_answer / 2))
-            for question, pred, label in zip(raw_qs, preds, test_batch.label):
-                eval_tuples.append((question, pred, label))
-        if args.store_eval_tuples:
-            _store_tuples(eval_tuples)
-
-    else:
-        eval_tuples = _get_stored_tuples()
-
+    iter = 0
+    checkpoint_interval = 10
+    checkpoint_path = Path(args.dataset) / "test_checkpoint.pt"
+    breakpoint()
+    if checkpoint_path.exists():
+        print("Restoring testing from checkpoint file...")
+        stored_data = torch.load(checkpoint_path)
+        iter = len(stored_data)
+        eval_tuples = stored_data
+        test_loader = Subset(test_loader, range(iter, len(test_loader.dataset)))
+    for test_batch in tqdm(test_loader, desc="Testing w/ Checkpoints"):
+        # TODO Remove
+        if iter > 2:
+            print("Ending early for debugging")
+            break
+        ###############
+        
+        new_qs = []
+        raw_qs = test_batch["question"]
+        for i, q in enumerate(test_batch["question"]):
+            # insert VectorRAG context
+            new_qs.append(
+                prompt_template.format(
+                    question=q, context="\n".join(test_batch.text_context[i])))
+        test_batch.question = new_qs
+        if args.skip_graph_rag:
+            test_batch.desc = ""
+        preds = (inference_step(model, test_batch,
+                                max_out_tokens=max_chars_in_train_answer / 2))
+        for question, pred, label in zip(raw_qs, preds, test_batch.label):
+            eval_tuples.append((question, pred, label))
+        iter += 1
+        if iter % checkpoint_interval == 0:
+            torch.save(eval_tuples, checkpoint_path)
+    torch.save(eval_tuples, checkpoint_path)
+    
     scores = []
     summaries = {}
-    for i, (question, pred, label) in enumerate(tqdm(eval_tuples, desc="Eval")):
+    summaries['args'] = vars(args)
+    
+    iter = 0
+    json_path = Path(args.dataset) / "eval_dump.json"
+    if json_path.exists():
+        with open(json_path) as f:
+            summaries = json.load(f)
+            iter = len(summaries) - 1
+
+    for question, pred, label in tqdm(eval_tuples, desc="Eval"):
         score, summary = eval(question, pred, label)
         scores.append(score)
         summaries[i] = summary
+        
+        iter += 1
+        if iter % checkpoint_interval == 0:
+            dump_eval_results(args, summaries)
+        
     avg_scores = sum(scores) / len(scores)
     print("Avg marlin accuracy=", avg_scores)
     print("*" * 5 + "NOTE" + "*" * 5)
     print("Marlin Accuracy is Estimated by LLM as a Judge!")
     print("Improvement of this estimation process is WIP...")
-    
-    return summaries
+    dump_eval_results(args, summaries)
 
 
 if __name__ == '__main__':
@@ -819,5 +806,4 @@ if __name__ == '__main__':
                              drop_last=False, pin_memory=True, shuffle=False)
 
     model = train(args, train_loader, val_loader)
-    res = test(model, test_loader, args)
-    dump_eval_results(args, res)
+    test(model, test_loader, args)
